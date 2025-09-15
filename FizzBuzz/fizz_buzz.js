2761bcc793486275b23df86d183958fb51f652c1
PabloPascucci <pascuccipablo20@gmail.com>
2025-09-15 07:15:26 -0300

    FizzBuzz punto y coma

--- FizzBuzz/fizz_buzz.js
+++ FizzBuzz/fizz_buzz.js
@@ -2,18 +2,18 @@
 // Subido a GitHub: 15/09/2025
 // Actualizado en GitHub: //
 
-console.log("Si ves este log, el JS esta enlazado")
+console.log("Si ves este log, el JS esta enlazado");
 
 for (i = 1; i < 101; i++) {
-    let divisibleThree = i % 3 == 0
-    let divisibleFive = i % 5 == 0
+    let divisibleThree = i % 3 == 0;
+    let divisibleFive = i % 5 == 0;
 
     if(divisibleThree && divisibleFive){
-        console.log("fizzbuzz")
+        console.log("fizzbuzz");
     } else if(divisibleThree) {
         console.log("fizz");
     } else if(divisibleFive) {
-        console.log("buzz")
+        console.log("buzz");
     } 
     else {
         console.log(i);
